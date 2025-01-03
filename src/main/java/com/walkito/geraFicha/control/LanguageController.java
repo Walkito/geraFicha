c2697bbae64c43e983fa961bfda92b3924027e25
Edamatec <119522693+focaG@users.noreply.github.com>
2025-01-03 15:38:56 -0300

    Entities

---
+++ src/main/java/com/walkito/geraFicha/control/LanguageController.java
@@ -0,0 +1,22 @@
+package com.walkito.geraFicha.control;
+
+import com.walkito.geraFicha.model.ApiResponse;
+import com.walkito.geraFicha.model.Language.Language;
+import com.walkito.geraFicha.service.LanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/language")
+public class LanguageController {
+    @Autowired
+    LanguageService languageService;
+
+    @PostMapping
+    public ApiResponse createLanguage(@RequestBody Language language){
+        return languageService.createLanguage(language);
+    }
+}
